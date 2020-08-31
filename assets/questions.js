b80193246119fe495977a9124c94e5958398eb70
Modernwyatt <60499726+Modernwyatt@users.noreply.github.com>
2020-08-30 21:52:01 -0600

    final code

--- assets/questions.js
+++ assets/questions.js
@@ -1,14 +1,13 @@
   // Basis of code taken from homework README.
   const questions = [
-    
     {
       title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts",
+      choices: ["strings", "booleans", "alerts",    "numbers"],
+      answer: "C",
       userAnswer: "alerts",
       outcome: false,
       time: 0
-    },
+      },
     {
       title: "The condition in an if / else statement is enclosed within ____.",
       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
--- assets/script.js
+++ assets/script.js
@@ -1,66 +1,204 @@
-// pos is position of where the user in the test or which question they're up to
-var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, chD, correct = 0;
-var startQuizEl = document.getElementById
-('startQuiz')
-var qDivEl = document.getElementById('qDiv')
-// this is a multidimensional array with 4 inner array elements with 5 elements inside the
-// this get function is short for the getElementById function  
-function get(x){
-return document.getElementById(x);
-}
-// this function renders a question for display on the page
+function initQuiz() {
+  //  Initialize "time remaining" variable
+      let timeRemaining=0;
+  
+      
+  //  Clicking the "Start Quiz" button starts the quiz, hides the landing container, and displays the quiz container
+      const startButtonEl = document.getElementById("start-button");
+      const timeRemainingEl = document.getElementById("time-remaining");
+      const finalScoreEl = document.getElementById("final-score");
+      const numQuestions = questions.length;
+      const landingContainerEl = document.getElementById("landing-container");
+      const quizContainerEl = document.getElementById("quiz-container");
+      const finalContainerEl = document.getElementById("final-container");
+      const submitButtonEl = document.getElementById("submit-initials");
+      const highscoreButtonEl = document.getElementById("highscore-button");
+      const highscoreContainerEl = document.getElementById("highscore-container");
+      let highScores = [];
+          //  Method to store and retrieve arrays in/from local storage obtained from https://stackoverflow.com/questions/3357553/how-do-i-store-an-array-in-localstorage
+      if (JSON.parse(localStorage.getItem('scores')) !== null) {
+          highScores = JSON.parse(localStorage.getItem("scores"));
+      }
+  
+      function startQuiz() {
+          
+          
+          landingContainerEl.setAttribute("class","container d-none");
+          let rowEl = null;
+          let colEl = null;
+          let headerEl = null;
+          let buttonEl = null;
+          quizContainerEl.setAttribute("class","container");
+          let currentQuestion = 1;
+          let score = 0;
+  //  Upon starting the quiz, the time remaining variable is assigned a value equal to 15 seconds * the number of questions and starts decreasing by 1 each second
+          timeRemaining=numQuestions * 15;
+          timeRemainingEl.setAttribute("value",timeRemaining);
+          //  Method for stopping the interval once it has started obtained from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Timeouts_and_intervals
+          let myInterval = setInterval(function() {
+              if (timeRemaining<1) {
+                  clearInterval(myInterval);
+                  //  When the final question is answered or the timer reaches zero, the quiz container is hidden and the score container is displayed, where the user enters their initials
+                  quizContainerEl.setAttribute("class","container d-none");
+                  finalContainerEl.setAttribute("class","container");
+                  return;
+              }
+              timeRemaining = timeRemaining - 1;
+              timeRemainingEl.setAttribute("value",timeRemaining);
+          },1000);
+          let clickTimeout = false;
+          function generateQuestion(questionNum) {
+              //  During the quiz, the header has the current question, and the answer buttons have the possible answers for that question
+              quizContainerEl.innerHTML = "";
+              rowEl = document.createElement("div");
+              rowEl.setAttribute("class","row");
+              quizContainerEl.append(rowEl);
 
+              colEl = document.createElement("div");
+              colEl.setAttribute("class","col-0 col-sm-2");
+              rowEl.append(colEl);
 
-function renderQuestion(){
-test = get("test");
-if(pos >= questions.length){
-    test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
-    get("test_status").innerHTML = "Test completed";
-    // resets the variable to allow users to restart the test
-    pos = 0;
-    correct = 0;
-    // stops rest of renderQuestion function running when test is completed
-    return false;
-}
-get("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;  
-question = questions[pos].title;
-chA = questions[pos].choices[0];
-chB = questions[pos].choices[1];
-chC = questions[pos].choices[2];
-chD = questions[pos].choices[3];
-  // display the question
-test.innerHTML = "<h3>"+question+"</h3>";
-  // display the answer options
-  // the += appends to the data we started on the line above
-test.innerHTML += "<label> <input type='radio' name='choices' value='A'> "+chA+"</label><br>";
-test.innerHTML += "<label> <input type='radio' name='choices' value='B'> "+chB+"</label><br>";
-test.innerHTML += "<label> <input type='radio' name='choices' value='C'> "+chC+"</label><br>";
-test.innerHTML += "<label> <input type='radio' name='choices' value='D'> "+chD+"</label><br><br>";
-test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
-}
-function unHide() {
-    qDivEl.removeAttribute('class', 'hide')
-    renderQuestion() 
-}
-function checkAnswer(){
-  // use getElementsByName because we have an array which it will loop through
-choices = document.getElementsByName("choices");
-for(var i=0; i<choices.length; i++){
-if(choices[i].checked){
-choice = choices[i].value;
-}
-}
-  // checks if answer matches the correct choice
-if(choice == questions[pos].answer){
-    //each time there is a correct answer this value increases
-correct++;
-}
-  // changes position of which character user is on
-pos++;
-  // then the renderQuestion function runs again to go to next question
-renderQuestion();
-}
-// Add event listener to call renderQuestion on page load event
-// we can add onclick event listener
-//window.addEventListener("load", renderQuestion);
-startQuizEl.addEventListener('click', unHide)
+              colEl = document.createElement("div");
+              colEl.setAttribute("class","col-12 col-sm-8");
+              rowEl.append(colEl);
+
+              colEl = document.createElement("div");
+              colEl.setAttribute("class","col-0 col-sm-2");
+              rowEl.append(colEl);
+
+              colEl = rowEl.children[1];
+              rowEl = document.createElement("div");
+              rowEl.setAttribute("class","row mb-3");
+              colEl.append(rowEl);
+
+              colEl = document.createElement("div");
+              colEl.setAttribute("class","col-12");
+              rowEl.append(colEl);
+
+              headerEl = document.createElement("h2");
+              headerEl.innerHTML = questions[questionNum-1].title;
+              colEl.append(headerEl);
+
+              colEl = quizContainerEl.children[0].children[1];
+              for (let i=0; i<4; i++) {
+                  let rowEl = document.createElement("div");
+                  rowEl.setAttribute("class","row mb-1");
+                  colEl.append(rowEl);
+
+                  let colEl2 = document.createElement("div");
+                  colEl2.setAttribute("class","col-12");
+                  rowEl.append(colEl2);
+
+                  buttonEl = document.createElement("button");
+                  buttonEl.setAttribute("class","btn btn-primary");
+                  buttonEl.setAttribute("type","button");
+                  buttonEl.innerHTML = questions[currentQuestion-1].choices[i];
+                  colEl2.append(buttonEl);
+                  buttonEl.addEventListener("click",function(){
+                      //  When the user clicks one of the answer buttons, if it is the correct answer, the message "Correct" is displayed, and if not, the message "Incorrect" is displayed and 15 seconds deducted from the timer
+                      if (clickTimeout) {
+                          return;
+                      }
+                      clickTimeout = true;
+                      clearInterval(myInterval);
+                      let colEl = quizContainerEl.children[0].children[1];
+                      let rowEl = document.createElement("div");
+                      rowEl.setAttribute("class","row border-top");
+                      colEl.append(rowEl);
+
+                      colEl = document.createElement("div");
+                      colEl.setAttribute("class","col-12");
+                      rowEl.append(colEl);
+
+                      let parEl = document.createElement("p");
+                      colEl.append(parEl);
+                      if (this.innerHTML === questions[currentQuestion - 1].answer) {
+                          parEl.innerHTML = "Correct!";
+                      } else {
+                          parEl.innerHTML = "Incorrect";
+                          timeRemaining = timeRemaining - 15;
+                          if (timeRemaining < 0) {
+                              timeRemaining = 0;
+                          }
+                          timeRemainingEl.setAttribute("value",timeRemaining);
+                      }
+                      currentQuestion++;
+                      if (currentQuestion>questions.length) {
+                          score = timeRemaining;
+                      }
+                      setTimeout(function() {
+                          // When an answer is chosen, pause the timer and show the result for 2 seconds before loading the next question
+                          if (currentQuestion>questions.length) {
+                              // Move to the results page
+                              quizContainerEl.setAttribute("class","container d-none");
+                              finalContainerEl.setAttribute("class","container");
+                              finalScoreEl.setAttribute("value",score);
+                          } else {
+                              generateQuestion(currentQuestion);
+                              clickTimeout = false;
+                              myInterval = setInterval(function() {
+                                  if (timeRemaining<1) {
+                                      clearInterval(myInterval);
+                                      quizContainerEl.setAttribute("class","container d-none");
+                                      finalContainerEl.setAttribute("class","container");
+                                      return;
+                                  }
+                                  timeRemaining = timeRemaining - 1;
+                                  timeRemainingEl.setAttribute("value",timeRemaining);
+                              },1000);
+                          }
+                      },2000);
+                  });
+              }
+              
+
+          }
+          function saveHighScore() {
+              let initialsEl = document.getElementById("initials-entry");
+              let newHighScore = {
+                  initials: initialsEl.value,
+                  highScore: score
+              };
+              console.log(newHighScore);
+              highScores.push(newHighScore);
+              console.log(highScores);
+              localStorage.setItem("scores",JSON.stringify(highScores));
+          }
+          submitButtonEl.addEventListener("click",saveHighScore);
+          
+          generateQuestion(currentQuestion);
+      }
+
+      startButtonEl.addEventListener("click",startQuiz);
+
+      highscoreButtonEl.addEventListener("click",function() {
+          landingContainerEl.setAttribute("class","container d-none");
+          quizContainerEl.setAttribute("class","container d-none");
+          finalContainerEl.setAttribute("class","container d-none");
+          highscoreContainerEl.setAttribute("class","container");
+          let colEl = document.getElementById("highscore-table");
+          for (i=0; i<highScores.length; i++) {
+              let rowEl = document.createElement("div");
+              rowEl.setAttribute("class","row mb-1");
+              colEl.append(rowEl);
+
+              let colEl2 = document.createElement("div");
+              colEl2.setAttribute("class","col-12 text-center");
+              rowEl.append(colEl2);
+
+              let parEl = document.createElement("div");
+              parEl.innerHTML = "Initials: " + highScores[i].initials + "   Score: " + highScores[i].highScore;
+              colEl2.append(parEl);
+          }
+      });
+  
+      
+  
+  
+  
+  
+  
+  
+  }
+  
+initQuiz();
